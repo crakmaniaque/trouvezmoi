1ec5dc6e6c6312f7f808a04f6961ef52c47b7446
crakmaniaque <crakmaniaque.assistance@gmail.com>
2015-07-08 18:02:04 -0400

    0.9.73.1
    Ajout de la statistique pour le nombre de nombres indices trouvés

---
+++ src/0.9.73.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Trouvez-moi!")]
+[assembly: AssemblyDescription("Jouer à Trouvez-moi!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("André-Luc Huneault")]
+[assembly: AssemblyProduct("Trouvez-moi!")]
+[assembly: AssemblyCopyright("Copyright © 2015 YAL informatique")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("c4b8ef2c-d28c-405f-ac0d-cd1936be8ec9")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.73.1")]
+[assembly: AssemblyFileVersion("0.9.73.1")]
